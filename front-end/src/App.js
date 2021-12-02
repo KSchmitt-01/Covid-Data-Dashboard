805e95151ae315c06111818a5e7b722281d0f1c6
andrewhaddon74 <86433050+AndrewHaddon74@users.noreply.github.com>
2021-12-01 22:38:13 -0700

    Renamed the table

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import DashboardView from './components/DashboardView';
 import VacinationTable from "./components/graphs/VacinationTable";
-import VacinationSmallTable from "./components/graphs/VacinationSmallTable"
+import VacinationSmallTable from "./components/graphs/VacinationTableTotalAndLatestWeek"
 
 function App() {
   return (
