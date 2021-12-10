74d5e0214bb9487655d5fce2b765b94ab130d73e
Kincaid Schmitt <kincaidschmitt@u.boisestate.edu>
2021-12-09 22:55:22 -0700

    Add feedback link

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Grid from '@material-ui/core/Grid'
 import Typography from "@mui/material/Typography";
+import Link from '@material-ui/core/Link'
 
 function Footer({mode}) {
     return (
@@ -18,13 +19,17 @@ function Footer({mode}) {
                 </Grid>
                 <Grid item xs={8} md={8} lg={8}>
                     <Typography align="right" variant="h6" sx={{color: 'white'}}>
-                        Created by: Andrew Haddon, Matthew Johnson, Kincaid Schmitt, Alyssa West
+                        Created by: Andrew Haddon, Matthew Johnson,
+                         Kincaid Schmitt, Alyssa West
                     </Typography>
                 </Grid>
                 <Grid item xs={2} md={2} lg={3}>
-                    <Typography align="right" variant="h6" sx={{color: 'white', paddingRight: '50px', verticalAlign: 'middle'}}>
+                    <Typography align="right" variant="h6" sx={{color: 'white', paddingRight: '103px', verticalAlign: 'middle'}}>
                         Boise State University CS354
                     </Typography>
+                    <Link color="#707de8" underline="always" variant="subtitle1" href="https://docs.google.com/forms/d/e/1FAIpQLScO6SDKNev0JpDRa4_21A8Hz_tLD5f98cBHHcauGDCl1abiZQ/viewform">
+                        Provide Feedback
+                    </Link>
                 </Grid>
             </Grid>
         </Box>
